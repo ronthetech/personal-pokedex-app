ed88fd0e0a14a1ac544e4898a9ce3359787d87a8
Ronald Jean-Francois <105710107+ronthetech@users.noreply.github.com>
2022-07-04 18:11:52 -0400

    added functions to get more or less entries

--- src/containers/AllPokemon.js
+++ src/containers/AllPokemon.js
@@ -1,17 +1,37 @@
 import React, { useEffect, useState } from "react";
 import PokeEntry from "../components/PokeEntry";
-import { getPokemons } from "../services/pokemons";
+import { getPokemons, getMorePokemons } from "../services/pokemons";
 
 import "../App.css";
 
 const AllPokemon = () => {
 	const [pokemons, setPokemons] = useState([]);
+	const [count, setCount] = useState(1);
 
 	useEffect(() => {
 		getPokemons().then((data) => {
 			setPokemons(data);
 		});
 	}, []);
+
+	const morePokemon = () => {
+		getMorePokemons(count + 19).then((data) => {
+			setPokemons(data);
+			setCount((prevCount) => {
+				return prevCount + 20;
+			});
+		});
+	};
+
+	const lessPokemon = () => {
+		getMorePokemons(count - 21).then((data) => {
+			setPokemons(data);
+			setCount((prevCount) => {
+				return prevCount - 20;
+			});
+		});
+	};
+
 	//console.log(Object.entries(pokemons)[3] && Object.entries(pokemons)[3][1]);
 	return (
 		<div className='entries'>
--- src/services/pokemons.js
+++ src/services/pokemons.js
@@ -4,7 +4,13 @@ export const getPokemons = async () => {
 	const response = await fetch(URL);
 	return await response.json();
 };
-
 // export const getPokemons = () => {
 // 	return fetch(URL).then((response) => response.json());
+// };
+
+export const getMorePokemons = (offset) => {
+	return fetch(`{URL}?offset=${offset}&limit=20`).then((response) => response.json());
+};
+// export const getMorePokemons = (offset) => {
+// 	return fetch(`{URL}?offset=${offset}&limit=20`).then((response) => response.json());
 // };
