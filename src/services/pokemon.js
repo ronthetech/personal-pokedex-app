d52cbac94bf4d41319d1f58c92ec55f552756ab0
Ronald Jean-Francois <105710107+ronthetech@users.noreply.github.com>
2022-07-04 17:41:09 -0400

    converted functions in services to async

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,5 +1,10 @@
 const URL = "https://pokeapi.co/api/v2";
 
-export const getPokemon = (pathName) => {
-	return fetch(`${URL}${pathName}`).then((response) => response.json());
+export const getPokemon = async (pathName) => {
+	const response = await fetch(`${URL}${pathName}`);
+	return await response.json();
 };
+
+// export const getPokemon = (pathName) => {
+// 	return fetch(`${URL}${pathName}`).then((response) => response.json());
+// };
--- src/services/pokemons.js
+++ src/services/pokemons.js
@@ -1,7 +1,8 @@
 const URL = "https://pokeapi.co/api/v2/pokemon";
 
-export const getPokemons = () => {
-	return fetch(URL).then((response) => response.json());
+export const getPokemons = async () => {
+	const response = await fetch(URL);
+	return await response.json();
 };
 
 // export const getPokemons = () => {
